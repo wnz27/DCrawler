284dbbb01d872bcf0e1bcf764d910a7ca118aaeb
fzk27 <wnz27@icloud.com>
2021-07-23 02:13:51 +0800

    udpate commment

--- zhenai/parser/city.go
+++ zhenai/parser/city.go
@@ -18,7 +18,8 @@ func ParseCity(contents []byte) engine.ParseResult {
 	matches := re.FindAllSubmatch(contents, -1)
 	result := engine.ParseResult{}
 	for _, m := range matches {
-
+		name := string(m[2])  // 整个copy， m 共享一个地址，每层循环复用这个m 使用解析函数不是立即，
+		// 而是后面引擎run的时候，那时候的跑的m都是一样的
 		result.Items = append(result.Items, "User: " + string(m[2]))
 		// todo 最简单的办法在这里直接把人的信息拿出来
 		result.Requests = append(result.Requests,
@@ -26,7 +27,7 @@ func ParseCity(contents []byte) engine.ParseResult {
 				Url: string(m[1]),
 				// 函数式编程，闭包的使用
 				ParserFunc: func(c []byte) engine.ParseResult {
-					return ParseProfile2(c, string(m[2]))
+					return ParseProfile2(c, name)
 				},
 			})
 	}
